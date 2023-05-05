e4770ec3c960d2e408d2e2c2d9baf4d234fc955a
KaiqueFj <kaiquelferraz@hotmail.com>
2023-05-05 19:21:18 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,152 @@
-import Image from 'next/image'
+import {
+  HomeIcon,
+  SearchIcon,
+  LibraryIcon,
+  ChevronDown,
+  ChevronRight,
+  ChevronLeft,
+} from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className="bg-black  h-screen flex flex-col  p-3">
+      <div className="flex flex-1">
+        <aside className=" w-160 p-6 bg-zinc-950  rounded-xl">
+          <nav className=" space-y-4">
+            <a
+              href=""
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-100"
+            >
+              <HomeIcon />
+              Home
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-100 "
+            >
+              <SearchIcon />
+              Search
+            </a>
+            <a
+              href=""
+              className="flex items-center gap-2 text-sm font-semibold text-zinc-100"
+            >
+              <LibraryIcon />
+              Your Library
+            </a>
+
+            <div className="  py-3 flex justify-start gap-4">
+              <a
+                href=""
+                className=" text-zinc-100 text-center bg-zinc-600 rounded-xl  p-1  "
+              >
+                Playlists
+              </a>
+              <a
+                href=""
+                className="   text-zinc-100 text-center bg-zinc-600  rounded-xl p-1  "
+              >
+                Albums
+              </a>
+              <a
+                href=""
+                className="   text-zinc-100 text-center bg-zinc-600 rounded-xl   p-1 "
+              >
+                Artists
+              </a>
+            </div>
+
+            <div className="py-3 flex justify-between gap-4 ">
+              <a href="" className=" text-zinc-100 text-left ">
+                <SearchIcon />
+              </a>
+
+              <a href="" className=" flex text-zinc-100  ">
+                Recent
+                <ChevronDown />
+              </a>
+            </div>
+          </nav>
+
+          <nav className=" mt-8 pt-10 border-t border-zinc-700">
+            <div className="flex items-center gap-3 mb-6">
+              <a href="">
+                <Image
+                  className=" flex rounded-xl"
+                  src="/images/pac.webp"
+                  alt="Album image of shark Playlist"
+                  width={80}
+                  height={120}
+                />
+              </a>
+              <p className="  text-zinc-100 text-base font-semibold ">
+                Rap old school
+              </p>
+            </div>
+
+            <div className="flex items-center gap-3 mb-6">
+              <a href="">
+                <Image
+                  className=" flex rounded-xl"
+                  src="/images/soul.jpg"
+                  alt="Album image of shark Playlist"
+                  width={80}
+                  height={120}
+                />
+              </a>
+              <p className="  text-zinc-100 text-base font-semibold ">Soul </p>
+            </div>
+
+            <div className="flex items-center gap-3 mb-6">
+              <a href="">
+                <Image
+                  className=" flex rounded-xl"
+                  src="/images/classical.webp"
+                  alt="Album image of shark Playlist"
+                  width={80}
+                  height={120}
+                />
+              </a>
+              <p className="  text-zinc-100 text-base font-semibold ">
+                Classical
+              </p>
+            </div>
+
+            <div className="flex items-center gap-3 mb-6">
+              <a href="">
+                <Image
+                  className=" flex rounded-xl"
+                  src="/images/vintage.webp"
+                  alt="Album image of shark Playlist"
+                  width={80}
+                  height={120}
+                />
+              </a>
+              <p className="  text-zinc-100 text-base font-semibold ">
+                Vintage
+              </p>
+            </div>
+          </nav>
+        </aside>
+
+        <main className="flex-1 bg-gradient-to-b from-zinc-700 via-zinc-900 to-zinc-900 rounded-xl">
+          <nav className="flex gap-2 p-4">
+            <a className="rounded-full bg-zinc-900  text-zinc-100 text-lg p-2">
+              <ChevronLeft />
+            </a>
+
+            <a className="rounded-full bg-zinc-900  text-zinc-100 text-lg p-2">
+              <ChevronRight />
+            </a>
+          </nav>
+
+          <div className="flex px-4 py-2 ">
+            <h3 className="text-zinc-100 text-3xl font-bold">Good Evening</h3>
+          </div>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <footer className="flex-1"> footer </footer>
+    </div>
+  );
 }
